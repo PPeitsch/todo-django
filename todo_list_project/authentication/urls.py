674e0024e187eac3d2f29dcb3ececdf218324fa9
PPeitsch <pablo.peitsch@gmail.com>
2024-09-16 08:35:07 -0300

    fixed authentication

--- todo_list_project/authentication/urls.py
+++ todo_list_project/authentication/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import SignUpView, CustomLoginView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+from .views import SignUpView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    # Add these for password reset functionality
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
