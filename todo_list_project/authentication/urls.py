5119aa516cf50d867af5160439e16827f37112ad
PPeitsch <pablo.peitsch@gmail.com>
2024-09-16 09:06:42 -0300

    fixed authentication logout

--- todo_list_project/authentication/urls.py
+++ todo_list_project/authentication/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from .views import CustomLoginView
-from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
-from .views import SignUpView
+from .views import SignUpView, CustomLogoutView
 
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
     # Add these for password reset functionality
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
--- todo_list_project/authentication/views.py
+++ todo_list_project/authentication/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 import logging
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.views import View
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +33,12 @@ class CustomLoginView(LoginView):
         response = super().form_valid(form)
         logger.info(f"User logged in: {self.request.user.username}")
         return response
+
+class CustomLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, "You have been successfully logged out.")
+        return redirect('home')  # o 'login' si prefieres redirigir al login
+
+    def post(self, request):
+        return self.get(request)
