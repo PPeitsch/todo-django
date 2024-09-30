66c9987ea342ff27b7223a3393c21094eefbffcc
PabloPeitsch <pablo.peitsch@gmail.com>
2024-09-30 17:10:59 -0300

    improved tests

--- todo_list_project/tasks/tests.py
+++ todo_list_project/tasks/tests.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Task
 from django.test import override_settings
+from django.utils.translation import gettext as _
 
 
 def ensure_unique_timestamps(func):
@@ -34,9 +35,9 @@ class TaskModelTest(TestCase):
 
     @ensure_unique_timestamps
     def test_task_creation(self):
-        task = self.create_task('Tarea de Prueba', 'Esta es una tarea de prueba')
-        self.assertEqual(task.title, 'Tarea de Prueba')
-        self.assertEqual(task.description, 'Esta es una tarea de prueba')
+        task = self.create_task('Test Task', 'This is a test task')
+        self.assertEqual(task.title, 'Test Task')
+        self.assertEqual(task.description, 'This is a test task')
         self.assertEqual(task.user, self.user)
         self.assertFalse(task.completed)
         self.assertIsNotNone(task.created_at)
@@ -44,13 +45,13 @@ class TaskModelTest(TestCase):
 
     @ensure_unique_timestamps
     def test_task_str_representation(self):
-        task = self.create_task('Tarea de Prueba')
-        self.assertEqual(str(task), 'Tarea de Prueba')
+        task = self.create_task('Test Task')
+        self.assertEqual(str(task), 'Test Task')
 
     @ensure_unique_timestamps
     def test_task_ordering(self):
-        task1 = self.create_task('Tarea 1')
-        task2 = self.create_task('Tarea 2')
+        task1 = self.create_task('Task 1')
+        task2 = self.create_task('Task 2')
         tasks = Task.objects.all()
         self.assertEqual(tasks[0], task2)
         self.assertEqual(tasks[1], task1)
@@ -72,12 +73,12 @@ class TaskViewsTest(TestCase):
 
     @ensure_unique_timestamps
     def test_task_list_view(self):
-        task1 = self.create_task('Tarea de Prueba 1')
-        task2 = self.create_task('Tarea de Prueba 2')
+        task1 = self.create_task('Test Task 1')
+        task2 = self.create_task('Test Task 2')
         response = self.client.get(reverse('task_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Tarea de Prueba 1')
-        self.assertContains(response, 'Tarea de Prueba 2')
+        self.assertContains(response, 'Test Task 1')
+        self.assertContains(response, 'Test Task 2')
         self.assertTemplateUsed(response, 'tasks/tasks_list.html')
         tasks = list(response.context['tasks'])
         self.assertEqual(tasks[0], task2)
@@ -86,12 +87,12 @@ class TaskViewsTest(TestCase):
     @ensure_unique_timestamps
     def test_task_create_view(self):
         response = self.client.post(reverse('task_create'), {
-            'title': 'Nueva Tarea',
-            'description': 'Esta es una nueva tarea'
+            'title': 'New Task',
+            'description': 'This is a new task'
         })
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(Task.objects.filter(title='Nueva Tarea').exists())
-        new_task = Task.objects.get(title='Nueva Tarea')
+        self.assertTrue(Task.objects.filter(title='New Task').exists())
+        new_task = Task.objects.get(title='New Task')
         self.assertEqual(new_task.user, self.user)
 
     @ensure_unique_timestamps
@@ -102,70 +103,73 @@ class TaskViewsTest(TestCase):
         })
         self.assertEqual(response.status_code, 200)
         self.assertFalse(Task.objects.filter(description='This task has no title').exists())
-        self.assertContains(response, 'Este campo es obligatorio.')
+        self.assertContains(response, 'This field is required.')
 
     @ensure_unique_timestamps
     def test_task_update_view(self):
-        task = self.create_task('Tarea Original')
+        task = self.create_task('Original Task')
         response = self.client.post(reverse('task_update', args=[task.id]), {
-            'title': 'Tarea Actualizada',
-            'description': 'Esta tarea ha sido actualizada'
+            'title': 'Updated Task',
+            'description': 'This task has been updated'
         })
         self.assertEqual(response.status_code, 302)
         task.refresh_from_db()
-        self.assertEqual(task.title, 'Tarea Actualizada')
-        self.assertEqual(task.description, 'Esta tarea ha sido actualizada')
+        self.assertEqual(task.title, 'Updated Task')
+        self.assertEqual(task.description, 'This task has been updated')
 
     @ensure_unique_timestamps
     def test_task_delete_view(self):
-        task = self.create_task('Tarea a Eliminar')
+        task = self.create_task('Task to Delete')
         response = self.client.post(reverse('task_delete', args=[task.id]))
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Task.objects.filter(id=task.id).exists())
 
     @ensure_unique_timestamps
     def test_task_toggle_complete_view(self):
-        task = self.create_task('Tarea Toggle')
+        task = self.create_task('Toggle Task')
+
+        # Toggle to complete
         response = self.client.post(reverse('task_toggle_complete', args=[task.id]))
         self.assertEqual(response.status_code, 302)
         task.refresh_from_db()
         self.assertTrue(task.completed)
 
-        response = self.client.post(reverse('task_toggle_complete', args=[task.id]))
+        # Toggle back to incomplete
+        self.client.post(reverse('task_toggle_complete', args=[task.id]))
         task.refresh_from_db()
         self.assertFalse(task.completed)
 
     @ensure_unique_timestamps
     def test_task_search(self):
-        self.create_task('Tarea de Prueba')
-        self.create_task('Otra Tarea')
-        response = self.client.get(reverse('task_list') + '?query=prueba')
-        self.assertContains(response, 'Tarea de Prueba')
-        self.assertNotContains(response, 'Otra Tarea')
+        self.create_task('Test Task')
+        self.create_task('Another Task')
+        response = self.client.get(reverse('task_list') + '?query=test')
+        self.assertContains(response, 'Test Task')
+        self.assertNotContains(response, 'Another Task')
 
     @ensure_unique_timestamps
     def test_task_list_ordering(self):
         Task.objects.all().delete()
-        self.create_task('Tarea A')
-        self.create_task('Tarea B')
+        self.create_task('Task A')
+        self.create_task('Task B')
         response = self.client.get(reverse('task_list'))
         tasks = list(response.context['tasks'])
-        self.assertEqual(tasks[0].title, 'Tarea B')
-        self.assertEqual(tasks[1].title, 'Tarea A')
+        self.assertEqual(tasks[0].title, 'Task B')
+        self.assertEqual(tasks[1].title, 'Task A')
 
     @ensure_unique_timestamps
     def test_task_list_chronological_order(self):
         Task.objects.all().delete()
-        self.create_task('Tarea 1')
-        self.create_task('Tarea 2')
-        self.create_task('Tarea 3')
+        self.create_task('Task 1')
+        self.create_task('Task 2')
+        self.create_task('Task 3')
 
         response = self.client.get(reverse('task_list'))
         tasks = list(response.context['tasks'])
 
-        self.assertEqual(tasks[0].title, 'Tarea 3')
-        self.assertEqual(tasks[1].title, 'Tarea 2')
-        self.assertEqual(tasks[2].title, 'Tarea 1')
+        self.assertEqual(tasks[0].title, 'Task 3')
+        self.assertEqual(tasks[1].title, 'Task 2')
+        self.assertEqual(tasks[2].title, 'Task 1')
 
 
 class TaskIntegrationTest(TestCase):
@@ -181,24 +185,24 @@ class TaskIntegrationTest(TestCase):
     def test_task_lifecycle(self):
         # Create a task
         response = self.client.post(reverse('task_create'), {
-            'title': 'Tarea de Prueba de Integración',
-            'description': 'Esta es una tarea de prueba de integración'
+            'title': 'Integration Test Task',
+            'description': 'This is an integration test task'
         })
         self.assertEqual(response.status_code, 302)
-        task = Task.objects.get(title='Tarea de Prueba de Integración')
+        task = Task.objects.get(title='Integration Test Task')
 
         # Verify task in list view
         response = self.client.get(reverse('task_list'))
-        self.assertContains(response, 'Tarea de Prueba de Integración')
+        self.assertContains(response, 'Integration Test Task')
 
         # Update the task
         response = self.client.post(reverse('task_update', args=[task.id]), {
-            'title': 'Tarea de Prueba de Integración Actualizada',
-            'description': 'Esta tarea ha sido actualizada'
+            'title': 'Updated Integration Test Task',
+            'description': 'This task has been updated'
         })
         self.assertEqual(response.status_code, 302)
         task.refresh_from_db()
-        self.assertEqual(task.title, 'Tarea de Prueba de Integración Actualizada')
+        self.assertEqual(task.title, 'Updated Integration Test Task')
 
         # Toggle task completion
         response = self.client.post(reverse('task_toggle_complete', args=[task.id]))
@@ -208,8 +212,8 @@ class TaskIntegrationTest(TestCase):
 
         # Verify completed task in list view
         response = self.client.get(reverse('task_list'))
-        self.assertContains(response, 'Tarea de Prueba de Integración Actualizada')
-        self.assertContains(response, 'Marcar como Incompleta')
+        self.assertContains(response, 'Updated Integration Test Task')
+        self.assertContains(response, 'Mark as Incomplete')
 
         # Delete the task
         response = self.client.post(reverse('task_delete', args=[task.id]))
@@ -218,7 +222,7 @@ class TaskIntegrationTest(TestCase):
 
         # Verify task no longer in list view
         response = self.client.get(reverse('task_list'))
-        self.assertNotContains(response, 'Tarea de Prueba de Integración Actualizada')
+        self.assertNotContains(response, 'Updated Integration Test Task')
 
 
 class TaskAccessControlTest(TestCase):
@@ -267,14 +271,37 @@ class TaskInternationalizationTest(TestCase):
 
     @override_settings(LANGUAGE_CODE='en')
     @ensure_unique_timestamps
-    def test_task_list_uses_translations(self):
+    def test_task_list_uses_english_translations(self):
         response = self.client.get(reverse('task_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'My Tasks')
 
+    @override_settings(LANGUAGE_CODE='es')
+    @ensure_unique_timestamps
+    def test_task_list_uses_spanish_translations(self):
+        response = self.client.get(reverse('task_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Mis Tareas')
+
     @override_settings(LANGUAGE_CODE='en')
     @ensure_unique_timestamps
-    def test_task_create_form_uses_translations(self):
+    def test_task_create_form_uses_english_translations(self):
         response = self.client.get(reverse('task_create'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Create Task')
+
+    @override_settings(LANGUAGE_CODE='es')
+    @ensure_unique_timestamps
+    def test_task_create_form_uses_spanish_translations(self):
+        response = self.client.get(reverse('task_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Crear Tarea')
+
+    def test_translation_consistency(self):
+        with override_settings(LANGUAGE_CODE='en'):
+            self.assertEqual(_('My Tasks'), 'My Tasks')
+            self.assertEqual(_('Create Task'), 'Create Task')
+
+        with override_settings(LANGUAGE_CODE='es'):
+            self.assertEqual(_('My Tasks'), 'Mis Tareas')
+            self.assertEqual(_('Create Task'), 'Crear Tarea')
